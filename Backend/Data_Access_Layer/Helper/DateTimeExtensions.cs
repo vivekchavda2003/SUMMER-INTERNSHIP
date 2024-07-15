2a0400554623fa28ada938063b85cb5d4e89ae68
vivekchavda2003 <167620037+vivekchavda2003@users.noreply.github.com>
2024-07-15 14:07:10 +0530

    first commit

---
+++ Backend/Data_Access_Layer/Helper/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.Helper
+{
+   
+        public static class DateTimeExtensions
+        {
+            public static DateTime ToUtc(this DateTime dateTime)
+            {
+                return dateTime.Kind == DateTimeKind.Unspecified
+                    ? DateTime.SpecifyKind(dateTime, DateTimeKind.Utc)
+                    : dateTime.ToUniversalTime();
+            }
+        }
+    
+
+}
